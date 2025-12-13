e31d6851269da6c353f43bf6327f8559cf7a90fc
ChocoChu32 <chocochu320@gmail.com>
2025-12-13 19:52:39 +0800

    添加主页模版

--- app.py
+++ app.py
@@ -1,27 +1,22 @@
-from flask import Flask, url_for
-from markupsafe import escape
+from flask import Flask, render_template
+
+name = "Choco Chu"
+movies = [
+    {"title": "肖申克的救赎", "year": "1994"},
+    {"title": "霸王别姬", "year": "1993"},
+    {"title": "泰坦尼克号", "year": "1997"},
+    {"title": "阿甘正传", "year": "1994"},
+    {"title": "千与千寻", "year": "2001"},
+    {"title": "美丽人生", "year": "1997"},
+    {"title": "星际穿越", "year": "2014"},
+    {"title": "这个杀手不太冷", "year": "1994"},
+    {"title": "盗梦空间", "year": "2010"},
+    {"title": "楚门的世界", "year": "1998"},
+]
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello"
-
-
-@app.route("/user/<name>")
-def user_page(name):
-    return f"User: {escape(name)}"
-
-
-@app.route("/test")
-def test_url_for():
-    # 下面是一些调用示例（访问 http://localhost:5000/test 后在命令行窗口查看输出的 URL）：
-    print(url_for("hello"))  # 生成 hello 视图函数对应的 URL，将会输出：/
-    # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
-    print(url_for("user_page", name="greyli"))  # 输出：/user/greyli
-    print(url_for("user_page", name="peter"))  # 输出：/user/peter
-    print(url_for("test_url_for"))  # 输出：/test
-    # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
-    print(url_for("test_url_for", num=2))  # 输出：/test?num=2
-    return "Test page"
+def index():
+    return render_template("index.html", name=name, movies=movies)
